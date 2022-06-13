2c3bb643dc0354e88017a1a708574a1a9e295398
Andrew Ziegler <ziegler@HAL.local>
2022-06-13 18:06:57 -0400

    python package WIP

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
-        print(self.sourcedir)
 
 class CMakeBuild(build_ext):
+
     def run(self):
         try:
             out = subprocess.check_output(['cmake', '--version'])
@@ -23,18 +23,18 @@ class CMakeBuild(build_ext):
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
 
-        #if platform.system() == "Windows":
-        #    cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-        #    if cmake_version < '3.1.0':
-        #        raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
+
+        ####### first build nymph and scarab #################################
+
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DNymph_ENABLE_TESTING:bool=1',
+                      '-DNymph_ENABLE_PYTHON:bool=1',
+                      '-DScarab_BUILD_PYTHON:bool=1',
                       '-DTettigoniidae_ENABLE_PYTHON:bool=1',
                      ]
 
@@ -56,20 +56,28 @@ class CMakeBuild(build_ext):
         print("should make a cmake call:")
         print(' '.join(['cmake', ext.sourcedir] + cmake_args), 'cwd={}'.format(self.build_temp), 'env={}'.format(env))
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         print("should make a build call:")
         print(['cmake', '--build', '.'] + build_args, 'cwd={}'.format(self.build_temp))
         #subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
         subprocess.check_call(['make', 'install'], cwd=self.build_temp)
 
+        ######################################################################
+
+        ######## next build tettigoniidae #####################################
+
+        ######################################################################
+
+
 if __name__ == "__main__":
 
     setup(
-        ext_modules=[CMakeExtension('nymph', sourcedir='')],
+        ext_modules=[
+            CMakeExtension('tettigoniidae',sourcedir='.')
+            ],
         cmdclass=dict(build_ext=CMakeBuild),
         packages = find_packages(
             where='Python',
-            include=['tettigoniidae'],
+            include=['tettigoniidae']
         ),
         package_dir={'': 'Python'}
     )
